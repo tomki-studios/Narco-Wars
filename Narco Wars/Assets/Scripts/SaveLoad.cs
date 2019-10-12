eecd283086220fff101c6e0b5a01b7ad70d86685
MiPiNi <YouTubePortoPL@wp.pl>
2019-10-12 21:43:40 +0200

    Basic Scripts
    Added:
    -Saving and Loading script
    -Wallet script

---
+++ Narco Wars/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoad : MonoBehaviour
+{
+    public Wallet wallet; //initializing class with cash variable
+    void Awake()
+    {
+        Load(); //loading all variables on awake
+    }
+    public void Save() //saving variables to memory
+    {
+        PlayerPrefs.SetInt("cash", wallet.cash);
+    }
+    public void Load() //loading variables from memory and setting them to specified variable
+    {
+        if (PlayerPrefs.HasKey("cash")) //if any variable is already saved, load all
+        {
+            wallet.cash = PlayerPrefs.GetInt("cash");
+        }
+        else //else save all
+            Save();
+    }
+}
+//By Daniel "MiPiNi" Grala
---
+++ Narco Wars/Assets/Scripts/Wallet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wallet : MonoBehaviour
+{
+    public SaveLoad saveLoad; //initializing savig and loading script
+    public int cash = 100000; //starting cash
+    public Text txt; //debugging
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = cash.ToString(); //debbuging
+    }
+    public void Debug() //only debugging
+    {
+        cash -= 100;
+        saveLoad.Save();
+    }
+}
+//By Daniel "MiPiNi" Grala
