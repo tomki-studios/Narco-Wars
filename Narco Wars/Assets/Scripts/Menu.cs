b221f8c0c5d96217aaed00df0c860e2d40b48310
Pixsel89 <47630371+Pixsel89@users.noreply.github.com>
2019-10-27 12:40:31 +0100

    Skrypt Menu, Podstawowa wersja menu i pierwszej sceny

---
+++ Narco Wars/Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public int Game;
+    public Canvas Opitons;
+    public Canvas Menu1;
+    public Canvas Chose;
+
+	public void Play()
+    {
+        Menu1.enabled = false;
+        Chose.enabled = true;
+    }
+
+    public void LoadGame()
+    {
+        //Na razie puste bo nie umiem zrobić//
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: Game);
+    }
+
+    public void Settings()
+    {
+        Menu1.enabled = false;
+        Opitons.enabled = true;
+    }
+
+    public void Back()
+    {
+        Menu1.enabled = true;
+        Opitons.enabled = false;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
